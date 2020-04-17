c776bac62bfec5cac91dba1863894f745d4d90ab
Filip <f.dryja1@gmail.com>
2020-04-17 13:26:07 +0200

    w

--- app/src/main/java/com/example/projektinzynierski/MainActivity.java
+++ app/src/main/java/com/example/projektinzynierski/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.projektinzynierski;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +9,19 @@ import android.annotation.SuppressLint;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private EditText editTextInput;
     private DrawerLayout drawer;
@@ -27,25 +33,55 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AddFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_add);
+        }
+
 //        editTextInput = findViewById(R.id.edit_text_input);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_add:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddFragment()).commit();
+                break;
+            case R.id.nav_alarm:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AlarmFragment()).commit();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
 
     }
 
-    public void startService(View v){
+    public void startService(View v) {
         String input = editTextInput.getText().toString();
 
         Intent serviceIntent = new Intent(this, ExampleService.class);
@@ -53,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         startService(serviceIntent);
     }
 
-    public void stopService(View v){
+    public void stopService(View v) {
         Intent serviceIntent = new Intent(this, ExampleService.class);
         stopService(serviceIntent);
     }
